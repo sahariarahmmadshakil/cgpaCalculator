9e790f9e4bb357f02f313c56b4a6d33e1d14d7cf
sahariarahmmadshakil <info.sashakil@gmail.com>
2023-01-03 11:29:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,101 @@
+// man change hoile let hobe.
+// man change na hoile const hobe
+
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+// Calculate Data Stracture
+
+const stastics=55;
+const calculas =76;
+const datastracture=86;
+
+// stastics marks 
+let stasticsMarks=0;
+if (stastics > 0 && stastics < 33) {
+    console.log(stasticsMarks=0);
+}
+else if (stastics > 33 && stastics < 40) {
+    console.log(stasticsMarks=1);
+}
+else if (stastics >= 40 && stastics < 50) {
+    console.log(stasticsMarks=2);
+}
+else if (stastics >= 50 && stastics < 60) {
+    console.log(stasticsMarks=2.5);
+}
+else if (stastics >= 60 && stastics < 70) {
+    console.log(stasticsMarks=3);
+}
+else if (stastics >= 70 && stastics < 80) {
+    console.log(stasticsMarks=3.5);
+}
+else if (stastics >= 80 && stastics < 100) {
+    console.log(stasticsMarks=4);
+}
+else {
+    console.log("Please Enter Value between 0-100");
+}
+
+
+// calculas marks 
+let calculasMarks=0;
+if (calculas > 0 && calculas < 33) {
+    console.log(calculasMarks=0);
+}
+else if (calculas > 33 && calculas < 40) {
+    console.log(calculasMarks=1);
+}
+else if (calculas >= 40 && calculas < 50) {
+    console.log(calculasMarks=2);
+}
+else if (calculas >= 50 && calculas < 60) {
+    console.log(calculasMarks=2.5);
+}
+else if (calculas >= 60 && calculas < 70) {
+    console.log(calculasMarks=3);
+}
+else if (calculas >= 70 && calculas < 80) {
+    console.log(calculasMarks=3.5);
+}
+else if (calculas >= 80 && calculas < 100) {
+    console.log(calculasMarks=4);
+}
+else {
+    console.log("Please Enter Value between 0-100");
+}
+
+
+// DataStracture marks 
+let datastractureMarks=0;
+if (datastracture > 0 && datastracture < 33) {
+    console.log(datastractureMarks=0);
+}
+else if (datastracture > 33 && datastracture < 40) {
+    console.log(datastractureMarks=1);
+}
+else if (datastracture >= 40 && datastracture < 50) {
+    console.log(datastractureMarks=2);
+}
+else if (datastracture >= 50 && datastracture < 60) {
+    console.log(datastractureMarks=2.5);
+}
+else if (datastracture >= 60 && datastracture < 70) {
+    console.log(datastractureMarks=3);
+}
+else if (datastracture >= 70 && datastracture < 80) {
+    console.log(datastractureMarks=3.5);
+}
+else if (datastracture >= 80 && datastracture < 100) {
+    console.log(datastractureMarks=4);
+}
+else {
+    console.log("Please Enter Value between 0-100");
+}
+
+const cgpa=(datastractureMarks+calculasMarks+stasticsMarks)/3;
+console.log("Congraqtulations Your CGPA is:", cgpa.toFixed(2));
+
+
+
